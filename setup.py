428b9d39b347a619b8adcfb4ddfebeea2c451967
nashquant <matheus.augusto.pires@alumni.usp.br>
2021-08-02 19:47:49 -0300

    initiating...

---
+++ setup.py
@@ -0,0 +1,30 @@
+from sys import version_info
+from os.path import join, dirname
+
+if version_info < (3, 6):
+    raise Exception('VERSION ERROR: Python 3.6+ required')
+
+def read(fname:str) -> str:
+    
+    dir = dirname(__file__)
+    
+    return open(
+        join(dir, fname), 
+        encoding='utf-8'
+    ).read()
+
+if __name__ == '__main__':
+    from setuptools import setup, find_packages
+
+    setup(
+        name = "backtesthub",
+        license = 'AGPL-3.0',
+        author = "Matheus Pires",
+        author_email = "matheus.augusto.pires@alumni.usp.br",
+        description = "BacktestHub is a python-based backtest"
+        " framework focused solely on the engine capabilities",
+        long_description = read('README.md'),
+        packages = find_packages(),
+        version = '4.0.0',
+        
+    )
